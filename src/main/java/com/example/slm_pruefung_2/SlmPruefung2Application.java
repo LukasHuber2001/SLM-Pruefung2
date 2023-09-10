7856db066960c28afe8c0db52a1abbce4f973c18
Lukas Huber <lukashuber2020@gmail.com>
2023-09-10 21:46:38 +0200

    first commit

---
+++ src/main/java/com/example/slm_pruefung_2/SlmPruefung2Application.java
@@ -0,0 +1,15 @@
+package com.example.slm_pruefung_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SlmPruefung2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmPruefung2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slm_pruefung_2/SlmPruefung2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_pruefung_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmPruefung2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
